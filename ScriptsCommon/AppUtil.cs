433c23b8b02d96b9bc512326bcff642e942ec620
Miho Ijuin <miiiiiiiii747@gmail.com>
2019-07-17 15:51:48 +0900

    ClickUp #z643y マテリアルへのイージング処理を共通モジュールに移行

--- ScriptsCommon/AppUtil.cs
+++ ScriptsCommon/AppUtil.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using DG.Tweening;
 
 public class AppUtil
 {
@@ -11,6 +12,20 @@ public class AppUtil
 
     public static IEnumerator Wait(float waitTime) {
         yield return new WaitForSeconds(waitTime);
+    }
+
+    public static SetMaterialFloat(Material mat, string property, float startValue, float endValue, float duration, Ease ease)
+    {
+        DOTween.To(
+            ()=> startValue,
+            x =>
+            {
+                startValue = x;
+                mat.SetFloat(property, startValue);
+            },
+            endValue,
+            duration
+        ).SetEase(ease);
     }
 }
 
